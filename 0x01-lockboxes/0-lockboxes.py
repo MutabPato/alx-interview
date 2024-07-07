da15c6b3aeb8313f16100f18fbd3a7a201691851
MutabPato <patrickmtbz@gmail.com>
2024-07-07 18:15:30 +0000

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+""" Lockboxes """
 
 
 def canUnlockAll(boxes):
+    """ determines if all the boxes can be opened """
     n = len(boxes)
     unlocked = [False] * n
     unlocked[0] = True
-    keys = [0] # Start with the key to the first box
+    keys = [0]  # Start with the key to the first box
 
     while keys:
         current_key = keys.pop()
